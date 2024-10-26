9529645128f478b0227d721562bb256dd0dcbd1a
Anahi Vera <168015268+AnahiVera@users.noreply.github.com>
2024-10-26 22:39:13 +0000

    funcionalidad lista

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,11 +3,8 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-dark bg-dark">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">Autentication System</span>
-				</Link>
 				<div className="ml-auto">
 					<Link to="/Signup">
 						<button className="btn btn-primary">Sign up here!</button>
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -4,14 +4,14 @@ import { Context } from "../store/appContext";
 
 // Reducer para manejar las credenciales
 const reducer = (state, action) => {
-    switch (action.type) {
-        case 'email':
-            return { ...state, email: action.value };
-        case 'password':
-            return { ...state, password: action.value };
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case 'email':
+			return { ...state, email: action.value };
+		case 'password':
+			return { ...state, password: action.value };
+		default:
+			return state;
+	}
 };
 
 export const Login = () => {
@@ -19,46 +19,46 @@ export const Login = () => {
 	const navigate = useNavigate()
 
 	useEffect(() => {
-        if (store?.access_token !== null) navigate('/Login')
-    }, [store, navigate])
-    
+		if (store?.access_token) navigate('/')
+	}, [store.access_token, navigate])
+
 	// Estado inicial de las credenciales usando useReducer
-    const [credentials, dispatch] = useReducer(reducer, {
-        email: '',
-        password: ''
-    })
+	const [credentials, dispatch] = useReducer(reducer, {
+		email: '',
+		password: ''
+	})
 
-    const handleSubmit = async e => {
-        e.preventDefault()
-        const resp = await actions.login(credentials)
-        if(resp) navigate('/')
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		const resp = await actions.login(credentials)
+		
 
-    }
+	}
 
 
 	return (
 		<div className="container">
 			<h1>Login</h1>
 			<form onSubmit={handleSubmit}>
-            <input 
-				type="email" 
-				name="email" 
-				id="email" 
-				placeholder='email' 
-				onChange={(e) => dispatch({ type: 'email', value: e.target.value })} 
-				value={credentials.email}/>
-            
-			<input 
-				type="password" 
-				name="password" 
-				id="password" 
-				placeholder='password' 
-				onChange={(e) => dispatch({ type: 'password', value: e.target.value })} 
-				value={credentials.password}/>
-				
-			
-			<button>Login</button>
-            </form>
+				<input
+					type="email"
+					name="email"
+					id="email"
+					placeholder='email'
+					onChange={(e) => dispatch({ type: 'email', value: e.target.value })}
+					value={credentials.email} />
+
+				<input
+					type="password"
+					name="password"
+					id="password"
+					placeholder='password'
+					onChange={(e) => dispatch({ type: 'password', value: e.target.value })}
+					value={credentials.password} />
+
+
+				<button type="submit">Login</button>
+			</form>
 		</div>
 	);
 };
